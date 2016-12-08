29cf7e4b186485b0e11814e5627795f932ab3684
wangquanjava <wangquanjava@163.com>
2016-12-08 09:41:43 +0800

    修改格式

--- src/main/java/com/git/exception/GlobeExceptionResolver.java
+++ src/main/java/com/git/exception/GlobeExceptionResolver.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 
